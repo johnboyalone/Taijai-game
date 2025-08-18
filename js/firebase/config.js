b735a42164be5e4007b6575114dbabc245096611
johnboyalone <73130409+johnboyalone@users.noreply.github.com>
2025-08-18 21:11:27 +0700

    Update config.js

--- js/firebase/config.js
+++ js/firebase/config.js
@@ -1,10 +1,6 @@
-// js/firebase/config.js
-
-// ประกาศตัวแปร db เพื่อให้ไฟล์อื่นในโมดูล Firebase ใช้งานร่วมกันได้
 export let db;
 
 export function initializeFirebase() {
-    // ใส่ข้อมูล Firebase Configuration จากโปรเจกต์เดิมของคุณ
     const firebaseConfig = {
       apiKey: "AIzaSyAAeQyoxlwHv8Qe9yrsoxw0U5SFHTGzk8o",
       authDomain: "taijai.firebaseapp.com",
@@ -14,6 +10,8 @@ export function initializeFirebase() {
       messagingSenderId: "262573756581",
       appId: "1:262573756581:web:c17bfc795b5cf139693d4c"
     };
-    firebase.initializeApp(firebaseConfig);
+    if (!firebase.apps.length) {
+        firebase.initializeApp(firebaseConfig);
+    }
     db = firebase.database();
 }
