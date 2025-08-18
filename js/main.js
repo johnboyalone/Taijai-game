e34f0170c76762bfffecac781d4ae1bc971df289
johnboyalone <73130409+johnboyalone@users.noreply.github.com>
2025-08-18 22:40:11 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,12 +1,11 @@
-// js/main.js (ทดสอบ import ที่ 3)
+// js/main.js (ทดสอบ import ที่ 4)
 
 import { showScreen } from './ui/core.js';
 import { initializeFirebase } from './firebase/config.js';
-import { initializeAudio } from './audio.js'; // <--- เปิดบรรทัดนี้
+import { initializeAudio } from './audio.js';
+import { setupInitialListeners } from './ui/eventListeners.js'; // <--- เปิดบรรทัดนี้
 
-// import { setupInitialListeners } from './ui/eventListeners.js';
-
-alert("Test 3: audio.js imported successfully!");
+alert("Test 4: eventListeners.js imported successfully!");
 
 /*
 function main() {
