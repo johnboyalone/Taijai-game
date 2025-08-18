fd72142a44e6466e0f2233db559799e809707c03
johnboyalone <73130409+johnboyalone@users.noreply.github.com>
2025-08-18 23:37:34 +0700

    Delete js/main.js

--- js/main.js
+++
@@ -1,9 +0,0 @@
-// js/main.js (Final Stand - Step 1)
-
-// เราจะ import แค่ 2 อย่างที่เรารู้ว่าปลอดภัย
-import { showScreen } from './ui/core.js';
-import { setupInitialListeners } from './ui/eventListeners.js';
-
-// เรียกใช้ฟังก์ชันหลัก
-setupInitialListeners();
-showScreen('splash');
